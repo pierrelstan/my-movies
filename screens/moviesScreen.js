85b7215359cf7534168ae559f397545d95906a17
Stanley <stanleypl75@gmail.com>
2021-02-09 16:29:36 -0400

    Create moviesScreens

---
+++ screens/moviesScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function MoviesScreen() {
+  return (
+    <View>
+      <Text>We are here to do it like so</Text>
+    </View>
+  );
+}
