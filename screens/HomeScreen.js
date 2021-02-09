1250dec2433667f2b37422630bd2269ec83fb60c
Stanley <stanleypl75@gmail.com>
2021-02-09 16:29:02 -0400

    Create HomeScreen"

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+import styled from 'styled-components';
+import Header from '../components/Header';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Header />
+    </View>
+  );
+}
