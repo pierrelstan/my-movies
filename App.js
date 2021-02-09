faa9c02d7cfcd9e2b404aa728aa9a706b7ebb052
Stanley <stanleypl75@gmail.com>
2021-02-09 16:36:05 -0400

    Remove Header & StyleSheet,Text,View

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './components/Header';
+
 import BottomNavigation from './Navigations/BottomNavigation';
 
 export default function App() {
