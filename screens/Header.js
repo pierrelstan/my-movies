427865c11fd06629e393daaf4f2f8fbfea7b1415
Stanley <stanleypl75@gmail.com>
2021-02-09 13:03:26 -0400

    Create Header

---
+++ screens/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Header() {
+  return (
+    <Header>
+      <Container></Container>
+    </Header>
+  );
+}
+const Header = styled.View`
+  height: 40;
+  background-color: #fff;
+`;
+const Container = styled.View``;
