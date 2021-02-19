4be6f2be395f260e66c56836fb99ecf7830be6b6
Stanley <stanleypl75@gmail.com>
2021-02-18 21:59:14 -0400

    Add SubHeader,ListItems

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar, Text } from 'react-native';
 import styled from 'styled-components';
+import BackgroundImage from '../components/BackgroundImage';
 import Header from '../components/Header';
+import ListItems from '../components/ListItems';
+import SubHeader from '../components/SubHeader';
 
 export default function HomeScreen() {
   return (
     <View>
       <Header />
+      <SubHeader />
+      {/* <BackgroundImage /> */}
+      <Text>Trending Now</Text>
+      <ListItems />
+      <StatusBar style='dark' statusBarStyle='auto' />
     </View>
   );
 }
