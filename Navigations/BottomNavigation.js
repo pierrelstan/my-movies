4adff74c89caf3965e3957985e7341231c6ab4f8
Stanley <stanleypl75@gmail.com>
2021-02-18 21:57:58 -0400

    Create MyTabBar

--- Navigations/BottomNavigation.js
+++ Navigations/BottomNavigation.js
@@ -1,15 +1,67 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import styled from 'styled-components';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MoviesScreen from '../screens/moviesScreen';
 import HomeScreen from '../screens/HomeScreen';
+
+function MyTabBar({ state, descriptors, navigation }) {
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            accessibilityRole='button'
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{ flex: 1 }}
+          >
+            <Text style={{ color: isFocused ? '#673ab7' : '#222' }}>
+              {label}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
 const Tab = createBottomTabNavigator();
 
 export default function BottomNavigation() {
   return (
-    <NavigationContainer>
+    <NavigationContainer tabBar={(props) => <MyTabBar {...props} />}>
       <Tab.Navigator>
         <Tab.Screen name='Home' component={HomeScreen} />
         <Tab.Screen name='Movies' component={MoviesScreen} />
