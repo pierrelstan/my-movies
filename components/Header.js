65a3144ef47cab65be705f17ff169a64b12afc98
Stanley <stanleypl75@gmail.com>
2021-02-18 21:54:38 -0400

    Add icons

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import styled from 'styled-components';
 import Constants from 'expo-constants';
 import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 export default function Header() {
   return (
@@ -10,34 +11,35 @@ export default function Header() {
         <Title>Lo</Title>
       </Main>
       <Nav>
-        <List>Tv Shows</List>
-        <List>Movies</List>
-        <List>My List</List>
+        <List>
+          <Ionicons name='search' size={24} color='#fff' />
+        </List>
+        <List>
+          <FontAwesome5 name='user-circle' size={24} color='#fff' />
+        </List>
       </Nav>
     </Container>
   );
 }
 const Container = styled.View`
   height: 40px;
-  background-color: #fff;
-  margin-top: ${Constants.statusBarHeight}px;
+  background-color: #333;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: space-between;
 `;
 const Main = styled.View`
-  flex: 1;
-  justify-content: center;
+  justify-content: flex-end;
   align-items: center;
 `;
 const Title = styled.Text`
   font-size: 32px;
-  color: #333;
+  color: #fff;
 `;
 const List = styled.Text`
   font-size: 16px;
   margin-left: 30px;
   margin-right: 20px;
-  color: #333;
+  color: #fff;
 `;
 
 const Nav = styled.View`
