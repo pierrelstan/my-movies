44db95fdbc606302522d5936ae8e4a0d843272d0
Stanley <stanleypl75@gmail.com>
2021-02-21 15:27:46 -0400

    Create moviesAction

---
+++ redux/actions/moviesAction.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { GET_MOVIES } from '../types/types';
+
+export function getMovies(page = 0) {
+  return function (dispatch) {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${
+      process.env.API_TOKEN
+    }&language=fr&page=${page + 1}`;
+    return axios.get(url).then((movies) => {
+      dispatch({
+        type: GET_MOVIES,
+        movies: [...movies.data.results],
+        page: movies.data.page,
+        totalPages: movies.data.total_pages,
+      });
+    });
+  };
+}
