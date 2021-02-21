fbc8eec0f6da3a6c4e3b9efea9800724274485a7
Stanley <stanleypl75@gmail.com>
2021-02-21 15:34:46 -0400

    Implement redux dispatch & useSelector

--- components/ListItems.js
+++ components/ListItems.js
@@ -1,40 +1,66 @@
 import React from 'react';
-import { View, Image, SafeAreaView, FlatList } from 'react-native';
+import {
+  View,
+  Image,
+  SafeAreaView,
+  FlatList,
+  Animated,
+  Dimensions,
+} from 'react-native';
 import styled from 'styled-components';
 import Constants from 'expo-constants';
+import { useDispatch, useSelector } from 'react-redux';
+import { OpenItem, CloseItem } from '../redux/actions/openitemAction';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const Item = ({ name, image }) => (
-  <View
-    style={{
-      margin: 0,
-      padding: 0,
-    }}
-  >
+const Item = ({ name, image, id }) => {
+  const dispatch = useDispatch();
+  const TopAnim = React.useRef(new Animated.Value(1)).current;
+  const { action } = useSelector((state) => ({
+    action: state.openitem.action,
+  }));
+  const ToggleOpenItem = () => {
+    console.log(id);
+
+    dispatch(OpenItem());
+  };
+  return (
     <View
       style={{
-        margin: 12,
+        margin: 0,
         padding: 0,
-        borderRadius: 150,
-        borderColor: '#e3e3',
-        borderWidth: 2,
       }}
     >
-      <Image
-        source={image}
+      <View
         style={{
-          width: 100,
-          height: 100,
+          margin: 12,
           padding: 0,
-          margin: 0,
-          borderRadius: 50,
+          borderRadius: 150,
+          borderColor: '#e3e3',
+          borderWidth: 2,
         }}
-      />
+      >
+        <TouchableOpacity onPress={() => ToggleOpenItem()}>
+          <Image
+            source={{ uri: `https://image.tmdb.org/t/p/w500/${image}` }}
+            style={{
+              width: 150,
+              height: 150,
+              padding: 0,
+              margin: 0,
+              borderRadius: 150,
+            }}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
-  </View>
-);
+  );
+};
 
-export default function ListItems() {
-  const renderItem = ({ item }) => <Item name={item.name} image={item.image} />;
+export default function ListItems({ movies }) {
+  const renderItem = ({ item }) => (
+    <Item name={item.name} image={item.poster_path} id={item.id} />
+  );
 
   return (
     <SafeAreaView>
@@ -47,8 +73,9 @@ export default function ListItems() {
         <FlatList
           horizontal={true}
           showsHorizontalScrollIndicator={false}
-          data={data}
+          data={movies}
           renderItem={renderItem}
+          onEndReachedThreshold={0.5}
           keyExtractor={(item) => item.id.toString()}
           style={{
             margin: 0,
@@ -59,31 +86,3 @@ export default function ListItems() {
     </SafeAreaView>
   );
 }
-
-let data = [
-  {
-    id: 0,
-    name: 'Product 1',
-    image: require('../assets/bg_2.png'),
-  },
-  {
-    id: 1,
-    name: 'Product 2',
-    image: require('../assets/bg_2.png'),
-  },
-  {
-    id: 2,
-    name: 'Product 3',
-    image: require('../assets/bg_2.png'),
-  },
-  {
-    id: 3,
-    name: 'Product 4',
-    image: require('../assets/bg_2.png'),
-  },
-  {
-    id: 4,
-    name: 'Product 5',
-    image: require('../assets/bg_2.png'),
-  },
-];
