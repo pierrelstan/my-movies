7475c8b326a49d9c0afc0cb0da9c95ad97b72eb4
Stanley <stanleypl75@gmail.com>
2021-02-22 20:33:49 -0400

    Remove comment

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,7 +9,6 @@ import {
 import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import Constants from 'expo-constants';
-import BackgroundImage from '../components/BackgroundImage';
 import Header from '../components/Header';
 import ListItems from '../components/ListItems';
 import SubHeader from '../components/SubHeader';
@@ -18,12 +17,13 @@ import { getTrendingMovies } from '../redux/actions/trendingMoviesAction';
 import { getUpcomingMovies } from '../redux/actions/upcomingMoviesAction';
 import { OpenItem, CloseItem } from '../redux/actions/moviesAction';
 import Item from '../components/Item';
+import Hero from '../components/Hero';
 
 const window = Dimensions.get('window');
 const screen = Dimensions.get('screen');
 let w;
-if (window.width >= 320) {
-  w = window.height / 4;
+if (screen.width >= 320) {
+  w = screen.height / 10;
 }
 
 export default function HomeScreen() {
@@ -41,31 +41,32 @@ export default function HomeScreen() {
   }, []);
 
   return (
-    <View>
+    <View
+      style={{
+        backgroundColor: '#E3EDF7',
+      }}
+    >
       <Item />
       <Header />
       <SubHeader />
-      {/* <BackgroundImage /> */}
-      <SafeAreaView>
-        <ScrollView style={{ marginBottom: w }}>
-          <View>
-            <Title>trending now</Title>
-            <ListItems movies={trendingMovies} />
-            <Title>most popular</Title>
-            <ListItems movies={movies} />
-            <Title>upcoming movies</Title>
-            <ListItems movies={upcomingMovies} />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-
+      <ScrollView style={{ marginBottom: w }}>
+        <Hero />
+        <View>
+          <Title>Trending Now</Title>
+          <ListItems movies={trendingMovies} />
+          <Title>Most Popular</Title>
+          <ListItems movies={movies} />
+          <Title>Upcoming Movies</Title>
+          <ListItems movies={upcomingMovies} />
+        </View>
+      </ScrollView>
       <StatusBar style='dark' statusBarStyle='auto' />
     </View>
   );
 }
 
 const Title = styled.Text`
-  font-size: 30px;
+  font-size: 18px;
   margin: 4px;
-  text-transform: uppercase;
+  font-weight: bold;
 `;
