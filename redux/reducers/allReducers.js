95fcdfaa710df36dbe02ef4c1254cbd1e07c69f6
Stanley <stanleypl75@gmail.com>
2021-02-22 20:27:06 -0400

    Add lastMovie & item

--- redux/reducers/allReducers.js
+++ redux/reducers/allReducers.js
@@ -3,8 +3,12 @@ import movies from '../reducers/moviesReducer';
 import trendingMovies from './trendingMoviesReducer';
 import upcomingMovies from './upcomingMoviesReducer';
 import openitem from './openitemReducer';
+import lastMovie from './latestMovieReducer';
+import item from './itemModalReducer';
 
 const allReducers = combineReducers({
+  item: item,
+  lastMovie: lastMovie,
   openitem: openitem,
   movies: movies,
   trendingMovies: trendingMovies,
