73244f3e72ec5f64f811dcbcb038a16ade01271b
Stanley <stanleypl75@gmail.com>
2021-02-25 00:14:45 -0400

    Add MyStack

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Provider } from 'react-redux';
-import BottomNavigation from './Navigations/BottomNavigation';
+import ScreenNavigation from './Navigations/BottomNavigation';
+import MyStack from './Navigations/ScreenNavigation';
 import store from './redux/store/store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <BottomNavigation />
+      <MyStack />
     </Provider>
   );
 }
