4ed42a7816fd68e58c69a52d78656944c7219016
Stanley <stanleypl75@gmail.com>
2021-03-01 19:54:05 -0400

    Add WebAPI

--- redux/actions/upcomingMoviesAction.js
+++ redux/actions/upcomingMoviesAction.js
@@ -1,10 +1,9 @@
-import axios from 'axios';
+import WebAPI from '../../assets/ServicesAxios/service';
 import { GET_UPCOMING_MOVIES } from '../types/types';
 
 export function getUpcomingMovies(page = 0) {
   return function (dispatch) {
-    const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.API_TOKEN}&language=en-US&page=1`;
-    return axios.get(url).then((movies) => {
+    return WebAPI.getUpcomingMovies(page).then((movies) => {
       dispatch({
         type: GET_UPCOMING_MOVIES,
         upcomingMovies: [...movies.data.results],
