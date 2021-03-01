6e9078e9fe0bc5b81b784fe3db0c34119e809e4b
Stanley <stanleypl75@gmail.com>
2021-03-01 19:32:56 -0400

    Add HomeStackNavigation

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Provider } from 'react-redux';
-import ScreenNavigation from './Navigations/BottomNavigation';
-import MyStack from './Navigations/ScreenNavigation';
 import store from './redux/store/store';
+import HomeStackNavigation from './Navigations/HomeStackNavigation';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <MyStack />
+      <HomeStackNavigation />
     </Provider>
   );
 }
