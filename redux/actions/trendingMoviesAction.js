856c3267fa4b820b3724281eca9045424ac263b3
Stanley <stanleypl75@gmail.com>
2021-03-01 19:54:53 -0400

    Add WebAPI

--- redux/actions/trendingMoviesAction.js
+++ redux/actions/trendingMoviesAction.js
@@ -1,13 +1,10 @@
 import axios from 'axios';
+import WebAPI from '../../assets/ServicesAxios/service';
 import { GET_MOVIES_TRENDING } from '../types/types';
 
 export function getTrendingMovies(page = 0) {
   return function (dispatch) {
-    const url = `
-    https://api.themoviedb.org/3/trending/movies/day?api_key=${
-      process.env.API_TOKEN
-    }&page=${page + 1}`;
-    return axios.get(url).then((movies) => {
+    return WebAPI.getTrendingMovies(page).then((movies) => {
       dispatch({
         type: GET_MOVIES_TRENDING,
         trendingMovies: [...movies.data.results],
