c7f3001c669f57d66cc812645991cab9f3adcb96
Stanley <stanleypl75@gmail.com>
2021-03-01 19:38:36 -0400

    Implement ScreenOrientation

--- screens/PreviewVideoScreen.js
+++ screens/PreviewVideoScreen.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { View } from 'react-native';
+import * as ScreenOrientation from 'expo-screen-orientation';
 import { WebView } from 'react-native-webview';
 import { getMovie } from '../redux/actions/movieAction';
 
 export default function PreviewVideo({ route, navigation }) {
+  useEffect(() => {
+    async function changeScreenOrientation() {
+      await ScreenOrientation.lockAsync(
+        ScreenOrientation.OrientationLock.LANDSCAPE_LEFT,
+      );
+    }
+    changeScreenOrientation();
+  }, []);
+
   const { movie } = useSelector((state) => ({
     movie: state.movie.movie[0] || 0,
   }));
 
   const { id } = route.params;
-
   const dispatch = useDispatch();
 
   React.useEffect(
