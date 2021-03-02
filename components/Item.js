d289a074ab3e6d6d4f4b4555690963e945cf0028
Stanley <stanleypl75@gmail.com>
2021-03-02 19:14:28 -0400

    Remove buttons

--- components/Item.js
+++ components/Item.js
@@ -8,14 +8,14 @@ import {
 } from 'react-native';
 import * as ScreenOrientation from 'expo-screen-orientation';
 import { Ionicons } from '@expo/vector-icons';
-import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import styled from 'styled-components';
 import { useSelector, useDispatch } from 'react-redux';
-import { OpenItem, CloseItem } from '../redux/actions/openitemAction';
+import { CloseItem } from '../redux/actions/openitemAction';
 import { useState } from 'react';
+import ItemButtons from './ItemButtons';
 
-const ScreenHeight = Dimensions.get('window').height;
+const ScreenHeight = Dimensions.get('screen').height;
 const screenWidth = Dimensions.get('window').width;
 
 function Item() {
@@ -62,29 +62,6 @@ function Item() {
   const toggleClose = () => {
     dispatch(CloseItem());
   };
-  const handlePreviewVideo = (id) => {
-    navigation.navigate('Trailer', { id: id });
-  };
-  const handleDetailsMovie = (data) => {
-    const {
-      id,
-      image,
-      description,
-      title,
-      voteCount,
-      voteAverage,
-      dateRelease,
-    } = data;
-    navigation.navigate('MovieScreen', {
-      id,
-      image,
-      description,
-      title,
-      voteCount,
-      voteAverage,
-      dateRelease,
-    });
-  };
   return (
     <AnimatedContainer style={{ transform: [{ translateY: TopAnim }] }}>
       <SafeAreaView>
@@ -129,33 +106,14 @@ function Item() {
             </View>
           </View>
 
-          <WrapperIcons>
-            <TouchableOpacity onPress={() => handlePreviewVideo(data.id)}>
-              <Button>
-                <Ionicons name='play' size={24} color='#333' />
-                <ButtonText>Play</ButtonText>
-              </Button>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => handlePreviewVideo(data.id)}>
-              <ContainerIcons>
-                <Feather name='play' size={24} color='#fff' />
-                <TextIcon> Review</TextIcon>
-              </ContainerIcons>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => handleDetailsMovie(data)}>
-              <ContainerIcons>
-                <Feather name='info' size={24} color='#fff' />
-                <TextIcon>Details &amp; More</TextIcon>
-              </ContainerIcons>
-            </TouchableOpacity>
-          </WrapperIcons>
+          <ItemButtons data={data} navigation={navigation} />
         </Content>
       </SafeAreaView>
     </AnimatedContainer>
   );
 }
 
-const ItemMemo = React.memo(Item, (prev, next) => {});
+const ItemMemo = React.memo(Item, () => {});
 export default ItemMemo;
 const Container = styled.View`
   position: absolute;
@@ -197,44 +155,10 @@ const CloseView = styled.View`
   box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
 `;
 
-const WrapperIcons = styled.View`
-  margin-top: 8px;
-  flex-direction: row;
-  justify-content: space-between;
-  align-content: center;
-  align-items: center;
-  padding: 8px;
-`;
 const Image = styled.Image`
   width: 100px;
   height: 150px;
   padding: 0;
   margin: 0;
   border-radius: 2px;
-`;
-
-const Button = styled.View`
-  color: #333;
-  background-color: #fff;
-  padding: 10px 45px;
-  flex-direction: row;
-  justify-content: center;
-  align-self: baseline;
-
-  border-radius: 3px;
-`;
-const ButtonText = styled.Text`
-  color: #333;
-  font-size: 18px;
-  font-weight: bold;
-`;
-
-const TextIcon = styled.Text`
-  font-size: 14px;
-  color: #fff;
-`;
-const ContainerIcons = styled.View`
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
 `;
