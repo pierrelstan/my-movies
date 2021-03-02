8746853a5b61f29e114e5171b285d4defaf92f2f
Stanley <stanleypl75@gmail.com>
2021-03-01 20:15:27 -0400

    Implement Screen Orientation &handleDetailsMovie

--- components/Item.js
+++ components/Item.js
@@ -6,19 +6,21 @@ import {
   View,
   SafeAreaView,
 } from 'react-native';
+import * as ScreenOrientation from 'expo-screen-orientation';
 import { Ionicons } from '@expo/vector-icons';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import styled from 'styled-components';
 import { useSelector, useDispatch } from 'react-redux';
 import { OpenItem, CloseItem } from '../redux/actions/openitemAction';
+import { useState } from 'react';
 
 const ScreenHeight = Dimensions.get('window').height;
 const screenWidth = Dimensions.get('window').width;
 
-export default function Item() {
+function Item() {
+  const [data, setData] = useState({});
   const navigation = useNavigation();
-
   const { action, item } = useSelector((state) => ({
     action: state.openitem.action,
     item: state.item,
@@ -27,29 +29,61 @@ export default function Item() {
   const TopAnim = useRef(new Animated.Value(ScreenHeight)).current;
 
   useEffect(() => {
+    async function changeScreenOrientation() {
+      await ScreenOrientation.lockAsync(
+        ScreenOrientation.OrientationLock.PORTRAIT,
+      );
+    }
+    changeScreenOrientation();
+
     let mounted = true;
     if (mounted) {
       if (action == 'OPEN_ITEM') {
         Animated.spring(TopAnim, {
           toValue: Math.round(ScreenHeight) / 2.3,
+          duration: 0,
           useNativeDriver: true,
         }).start();
       } else {
         Animated.spring(TopAnim, {
           toValue: ScreenHeight,
+          duration: 0,
           useNativeDriver: true,
         }).start();
       }
     }
+    setData(item);
     return () => {
       mounted = false;
+      setData({});
     };
-  }, [action]);
+  }, [action, item]);
+
   const toggleClose = () => {
     dispatch(CloseItem());
   };
   const handlePreviewVideo = (id) => {
     navigation.navigate('Trailer', { id: id });
+  };
+  const handleDetailsMovie = (data) => {
+    const {
+      id,
+      image,
+      description,
+      title,
+      voteCount,
+      voteAverage,
+      dateRelease,
+    } = data;
+    navigation.navigate('MovieScreen', {
+      id,
+      image,
+      description,
+      title,
+      voteCount,
+      voteAverage,
+      dateRelease,
+    });
   };
   return (
     <AnimatedContainer style={{ transform: [{ translateY: TopAnim }] }}>
@@ -80,7 +114,7 @@ export default function Item() {
             >
               <Image
                 source={{
-                  uri: `https://image.tmdb.org/t/p/w500/${item.image}`,
+                  uri: `https://image.tmdb.org/t/p/w500/${data.image}`,
                 }}
               />
             </View>
@@ -91,27 +125,29 @@ export default function Item() {
                 padding: 0,
               }}
             >
-              <Text numberOfLines={3}>{item.description}</Text>
+              <Text numberOfLines={3}>{data.description}</Text>
             </View>
           </View>
 
           <WrapperIcons>
-            <TouchableOpacity onPress={() => handlePreviewVideo(item.id)}>
+            <TouchableOpacity onPress={() => handlePreviewVideo(data.id)}>
               <Button>
                 <Ionicons name='play' size={24} color='#333' />
                 <ButtonText>Play</ButtonText>
               </Button>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => handlePreviewVideo(item.id)}>
+            <TouchableOpacity onPress={() => handlePreviewVideo(data.id)}>
               <ContainerIcons>
                 <Feather name='play' size={24} color='#fff' />
                 <TextIcon> Review</TextIcon>
               </ContainerIcons>
             </TouchableOpacity>
-            <ContainerIcons>
-              <Feather name='info' size={24} color='#fff' />
-              <TextIcon>Details &amp; More</TextIcon>
-            </ContainerIcons>
+            <TouchableOpacity onPress={() => handleDetailsMovie(data)}>
+              <ContainerIcons>
+                <Feather name='info' size={24} color='#fff' />
+                <TextIcon>Details &amp; More</TextIcon>
+              </ContainerIcons>
+            </TouchableOpacity>
           </WrapperIcons>
         </Content>
       </SafeAreaView>
@@ -119,14 +155,16 @@ export default function Item() {
   );
 }
 
+const ItemMemo = React.memo(Item, (prev, next) => {});
+export default ItemMemo;
 const Container = styled.View`
   position: absolute;
   background-color: white;
   width: 100%;
   height: 100%;
   z-index: 100;
-  border-top-left-radius: 25px;
-  border-top-right-radius: 25px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
 `;
 const AnimatedContainer = Animated.createAnimatedComponent(Container);
 const Logo = styled.Image``;
@@ -138,9 +176,9 @@ const Text = styled.Text`
 `;
 const Content = styled.View`
   height: ${Math.round(ScreenHeight)}px;
-  background: #333;
-  border-top-left-radius: 25px;
-  border-top-right-radius: 25px;
+  background: #444;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
   padding: 5px;
 `;
 const ContainerClose = styled.View`
@@ -156,7 +194,7 @@ const CloseView = styled.View`
   justify-content: center;
   align-items: center;
   border-radius: 22px;
-  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.15);
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
 `;
 
 const WrapperIcons = styled.View`
--- components/UpcomingMovies.js
+++ components/UpcomingMovies.js
@@ -1,19 +1,15 @@
 import React, { useEffect } from 'react';
-import styled from 'styled-components';
 import axios from 'axios';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
-import ListItems from './ListItems';
-import { getTrendingMovies } from '../redux/actions/trendingMoviesAction';
 import { FlatList } from 'react-native-gesture-handler';
 import renderItem from './ListItemsChildren';
-import { getMovies } from '../redux/actions/moviesAction';
 import { getUpcomingMovies } from '../redux/actions/upcomingMoviesAction';
 import axiosService from '../assets/ServicesAxios/axiosService';
 
 export default function UpcomingMovies() {
   const dispatch = useDispatch();
-  const { upcomingMovies, page, totalPages } = useSelector((state) => ({
+  const { upcomingMovies } = useSelector((state) => ({
     upcomingMovies: state.upcomingMovies.upcomingMovies,
     page: state.upcomingMovies.page,
     totalPages: state.upcomingMovies.totalPages,
