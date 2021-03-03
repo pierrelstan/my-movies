f6c39a63c3450763b51723c08801f5f335c8e97b
Stanley <stanleypl75@gmail.com>
2021-03-03 12:14:07 -0400

    Implement isLoading and failure

--- redux/reducers/trendingMoviesReducer.js
+++ redux/reducers/trendingMoviesReducer.js
@@ -1,21 +1,37 @@
-import { GET_MOVIES_TRENDING } from '../types/types';
+import {
+  GET_MOVIES_TRENDING_SUCCESS,
+  LOADING_MOVIES_TRENDING_START,
+  LOADING_MOVIES_TRENDING_FAILURE,
+} from '../types/types';
 
 const initialState = {
   trendingMovies: [],
   page: '',
   totalPages: '',
+  isLoading: false,
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case GET_MOVIES_TRENDING:
+    case GET_MOVIES_TRENDING_SUCCESS:
       return {
         ...state,
         trendingMovies: [...action.trendingMovies],
         page: action.page,
         totalPages: action.totalPages,
+        isLoading: false,
       };
 
+    case LOADING_MOVIES_TRENDING_START:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case LOADING_MOVIES_TRENDING_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+      };
     case 'RESET': {
       return initialState;
     }
--- redux/reducers/upcomingMoviesReducer.js
+++ redux/reducers/upcomingMoviesReducer.js
@@ -1,21 +1,37 @@
-import { GET_UPCOMING_MOVIES } from '../types/types';
+import {
+  GET_UPCOMING_MOVIES_SUCCESS,
+  LOADING_UPCOMING_MOVIES_START,
+  LOADING_UPCOMING_MOVIES_FAILURE,
+} from '../types/types';
 
 const initialState = {
   upcomingMovies: [],
   page: '',
   totalPages: '',
+  isLoading: false,
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case GET_UPCOMING_MOVIES:
+    case GET_UPCOMING_MOVIES_SUCCESS:
       return {
         ...state,
         upcomingMovies: [...action.upcomingMovies],
         page: action.page,
         totalPages: action.totalPages,
+        isLoading: false,
       };
 
+    case LOADING_UPCOMING_MOVIES_START:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case LOADING_UPCOMING_MOVIES_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+      };
     case 'RESET': {
       return initialState;
     }
