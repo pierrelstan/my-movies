c391bcffb64073e3b876b4f14d417adfec490a81
Stanley <stanleypl75@gmail.com>
2021-03-05 22:15:40 -0400

    Create allMovies

---
+++ screens/AllMovies.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function AllMovies() {
+  return <Container></Container>;
+}
+const Container = styled.View`
+  background-color: #444;
+  width: 100%;
+  height: 500px;
+`;
