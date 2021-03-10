ff9afbefaef31feee0c4d440075edd6988b56818
Stanley <stanleypl75@gmail.com>
2021-03-10 17:31:12 -0400

    Remove Picker

--- screens/AllMovies.js
+++ screens/AllMovies.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { Picker } from '@react-native-community/picker';
-
 export default function AllMoVies() {
   const [state, setState] = React.useState({
     typeOfMovies: 'all',
@@ -14,16 +12,6 @@ export default function AllMoVies() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Movies</Text>
-      <Picker
-        style={styles.picker}
-        itemStyle={styles.pickerItem}
-        selectedValue={state.language}
-        onValueChange={(itemValue) => handlePicker(itemValue)}
-      >
-        <Picker.Item label='All' value='all' />
-        <Picker.Item label='Movies' value='movie' />
-        <Picker.Item label='Tv' value='tv' />
-      </Picker>
     </View>
   );
 }
