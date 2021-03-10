b38d3dddcd6da9a66e696261264cbeeb4e9d2487
Stanley <stanleypl75@gmail.com>
2021-03-10 11:23:46 -0400

    Implement Picker

--- screens/AllMovies.js
+++ screens/AllMovies.js
@@ -1,11 +1,50 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-community/picker';
 
-export default function AllMovies() {
-  return <Container></Container>;
+export default function AllMoVies() {
+  const [state, setState] = React.useState({
+    typeOfMovies: 'all',
+  });
+
+  const handlePicker = (itemValue) => {
+    setState({ typeOfMovies: itemValue });
+  };
+  console.log(state);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Movies</Text>
+      <Picker
+        style={styles.picker}
+        itemStyle={styles.pickerItem}
+        selectedValue={state.language}
+        onValueChange={(itemValue) => handlePicker(itemValue)}
+      >
+        <Picker.Item label='All' value='all' />
+        <Picker.Item label='Movies' value='movie' />
+        <Picker.Item label='Tv' value='tv' />
+      </Picker>
+    </View>
+  );
 }
-const Container = styled.View`
-  background-color: #444;
-  width: 100%;
-  height: 500px;
-`;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  picker: {
+    width: 200,
+    backgroundColor: '#FFF0E0',
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  pickerItem: {
+    color: 'red',
+  },
+});
